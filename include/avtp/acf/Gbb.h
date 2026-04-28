e6b1a465796ab927c83acd8d8fe2d29e622b24b4
Adriaan Niess <adriaan.niess@de.bosch.com>
2026-04-28 08:55:41 +0200

    Improve docstrings
    Signed-off-by: Adriaan Niess <adriaan.niess@de.bosch.com>

--- include/avtp/acf/Gbb.h
+++ include/avtp/acf/Gbb.h
@@ -123,7 +123,7 @@ static const Avtp_FieldDescriptor_t __AVTP_GBB_FIELDS[AVTP_GBB_FIELD_MAX] =
  * Macro to set the value of a field in an ACF_GBB message header.
  * 
  * @note This macro should not be used directly, instead use the field specific
- * getter functions defined below.
+ * setter functions defined below.
  */
 #define __Avtp_Gbb_SetField(field, value) \
         (Avtp_SetField(__AVTP_GBB_FIELDS, AVTP_GBB_FIELD_MAX, (uint8_t*)msg, field, value))
